98212da877a48681b24d7fde5f0adee3f5e1783e
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-05-20 11:26:53 -0600

    Update blank.test.js
    updated tests to add a good fail for calculating the users age into their age on mercury venus mars and jupiter

--- __tests__/blank.test.js
+++ __tests__/blank.test.js
@@ -7,4 +7,12 @@ describe('Human',()=> {
     expect(human.earthYears).toEqual(34);
   });
 
+  test('should calculate Earth years into the other planet years; mercury, venus, mars, jupiter', ()=> {
+    const human = new Human(34);
+    expect(human.mercuryYears).toEqual(8);
+    expect(human.venusYears).toEqual(8);
+    expect(human.marsYears).toEqual(8);
+    expect(human.jupiterYears).toEqual(8);
+  });
+
 });
