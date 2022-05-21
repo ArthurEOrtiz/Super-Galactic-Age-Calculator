281527049e91c4b1f1478a3accea1b362343257f
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-05-21 12:33:26 -0600

    set up test for life clock

--- __tests__/human.test.js
+++ __tests__/human.test.js
@@ -3,27 +3,34 @@ import Human from '../src/js/human.js';
 describe('Human',()=> { 
 
   test('should correctly create a human object with Earth years and the user inputted age', ()=> {
+
     const human = new Human(34);
     expect(human.earthYears).toEqual(34);
+
   });
 
   test('should calculate Earth years into the other planet years; mercury, venus, mars, jupiter', ()=> {
+    
     const human = new Human(34);
     human.solarAgeCalc();
     expect(human.mercuryYears).toEqual(141);
     expect(human.venusYears).toEqual(54);
     expect(human.marsYears).toEqual(18);
     expect(human.jupiterYears).toEqual(2);
+
   });
 
   test('should record life factors of stress and activity levels from user input; low, medium, high', ()=>{
+    
     const human = new Human(34);
     human.lifeFactors("low","high");
     expect(human.stressLevel).toEqual("low");
     expect(human.activityLevel).toEqual("high");
+
   });
 
   test('should assign a death point score according to lifefactors', ()=>{
+    
     const userInputs = ["high", "medium", "low"];
     const cases = [[10,15,20],[5,10,15],[0,5,10]];
 
@@ -53,6 +60,25 @@ describe('Human',()=> {
         expect(human.expectedJupiterDeath).toEqual(Math.floor(cases[i][j] / 11.86) - human.jupiterYears);
         }
       }
+
+  test('should return years of life lived after death expectancy', ()=>{
+    const userInputs = ["high", "medium", "low"];
+
+    for (let i = 0; i < 3; i++){
+      for (let j = 0; j < 3; j++) {
+        const human = new Human(34);
+        human.lifeFactors(userInputs[i],userInputs[j]);
+        human.lifeClock();
+        expect(human.lifeAfterEarthDeath).toEqual(human.earthYears - human.deathAge);
+        expect(human.lifeAfterMercuryDeath).toEqual(human.mercuryYears - Math.floor(human.deathAge / .24));
+        expect(human.lifeAfterVenusDeath).toEqual(human.venusYears - Math.floor(human.deathAge / .62));
+        expect(human.lifeAfterMarsDeath).toEqual(human.marsYears - Math.floor(human.deathAge / 1.88));
+        expect(human.lifeAfterJupiterDeath).toEqual(human.jupiterYears - Math.floor(human.deathAge / 11.86));
+      }
+    }
+
+
+  });
 
   });
 
