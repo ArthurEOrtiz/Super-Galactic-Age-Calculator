318969439f4bb7645ed20a3824de2b3c9a44e729
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-05-21 08:39:09 -0600

    Adding UX/UI

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Human from '../src/js/human.js';
 
 $(document).ready(function(){
+  $('#userInputForm').submit(function(event){
+    event.preventDefault();
+    const userAge = parseInt($('#userAge').val());
 
-  
+    let human = new Human(userAge);
+
+    console.log(human);
+    
+  });
 });
--- src/js/human.js
+++ src/js/human.js
@@ -3,49 +3,49 @@ export default class Human {
     this.earthYears = age;
   }
 
-solarAgeCalc(){
-  this.mercuryYears = Math.floor(this.earthYears / .24);
-  this.venusYears = Math.floor(this.earthYears / .62);
-  this.marsYears = Math.floor(this.earthYears / 1.88);
-  this.jupiterYears = Math.floor(this.earthYears / 11.86);
-}
-
-
-lifeFactors(stress, activity){
-  this.stressLevel = stress;
-  this.activityLevel = activity;
-
-  this.deathPoints = 0;
-
-  if (this.stressLevel === "high") {
-    this.deathPoints += 10;
-  } else if (this.stressLevel === "medium") {
-    this.deathPoints += 5;
-  } else {
-    this.deathPoints += 0;
+  solarAgeCalc(){
+    this.mercuryYears = Math.floor(this.earthYears / .24);
+    this.venusYears = Math.floor(this.earthYears / .62);
+    this.marsYears = Math.floor(this.earthYears / 1.88);
+    this.jupiterYears = Math.floor(this.earthYears / 11.86);
   }
 
-  if (this.activityLevel === "low") {
-    this.deathPoints += 10;
-  } else if (this.activityLevel === "medium") {
-    this.deathPoints += 5;
-  } else  {
-    this.deathPoints += 0;
-  }
 
-} 
+  lifeFactors(stress, activity){
+    this.stressLevel = stress;
+    this.activityLevel = activity;
+
+    this.deathPoints = 0;
+
+    if (this.stressLevel === "high") {
+      this.deathPoints += 10;
+    } else if (this.stressLevel === "medium") {
+      this.deathPoints += 5;
+    } else {
+      this.deathPoints += 0;
+    }
+
+    if (this.activityLevel === "low") {
+      this.deathPoints += 10;
+    } else if (this.activityLevel === "medium") {
+      this.deathPoints += 5;
+    } else  {
+      this.deathPoints += 0;
+    }
+
+  } 
 
 
-deathClock(){
-  const deathAge = 80 - this.deathPoints;
-  this.solarAgeCalc();
-  this.lifeFactors();
-  
-  this.expectedEarthDeath = deathAge - this.earthYears;
-  this.expectedMercuryDeath = Math.floor(deathAge / .24) - this.mercuryYears;
-  this.expectedVenusDeath = Math.floor(deathAge / .62) - this.venusYears;
-  this.expectedMarsDeath = Math.floor(deathAge / 1.88) - this.marsYears;
-  this.expectedJupiterDeath = Math.floor(deathAge / 11.86) - this.jupiterYears;
+  deathClock(){
+    const deathAge = 80 - this.deathPoints;
+    this.solarAgeCalc();
+    this.lifeFactors();
+    
+    this.expectedEarthDeath = deathAge - this.earthYears;
+    this.expectedMercuryDeath = Math.floor(deathAge / .24) - this.mercuryYears;
+    this.expectedVenusDeath = Math.floor(deathAge / .62) - this.venusYears;
+    this.expectedMarsDeath = Math.floor(deathAge / 1.88) - this.marsYears;
+    this.expectedJupiterDeath = Math.floor(deathAge / 11.86) - this.jupiterYears;
 
   }
 
